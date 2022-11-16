ef444e18ba6cd189772b4e6da06bd58251f5f0a0
hasanyannar <hasanyannar@gmail.com>
2022-11-16 17:34:23 +0300

    EntityFramework

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -19,10 +19,44 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-        
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length>=2 && car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba adını ve fiyatını kontrol ediniz");
+            }
+
+            
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c=>c.BrandId==brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c=>c.ColorId==colorId);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,7 +10,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            carManager.Add(new Car {BrandId = 1, ColorId = 1, ModelYear=2020, DailyPrice=175000, Description="civic" });
 
             foreach (var car in carManager.GetAll())
             {
