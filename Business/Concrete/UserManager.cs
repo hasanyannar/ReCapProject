e8e82bd95f18303ae43fa53724a816dcabc2dbda
hasanyannar <hasanyannar@gmail.com>
2022-12-03 18:40:56 +0300

    ++Autofac, Validation

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,12 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constant;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +25,9 @@ namespace Business.Concrete
             _userDal = userDal;
         }
 
+        [ValidationAspect(typeof(UserValidator))]
         public IResult Add(User user)
         {
-            ValidationTool.Validate(new UserValidator(), user);
 
             _userDal.Add(user);
             return new SuccessResult(Messages.UserAdded);
