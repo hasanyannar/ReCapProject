dfecfcfd829b9eb1b71121f0aef9feaf367be394
hasanyannar <hasanyannar@gmail.com>
2022-12-12 19:30:01 +0300

    CarImagesAdded

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        IDataResult<List<Image>> GetAll();
+        IDataResult<List<Image>> GetByCarId(int carId);
+        IDataResult<Image> GetByImageId(int ImageId);
+        IResult Add(IFormFile file, Image carImage);
+        IResult Delete(Image carImage);
+        IResult Update(IFormFile file, Image carImage);
+    }
+}
---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,100 @@
+﻿using Business.Abstract;
+using Core.Utilities.Business;
+using Core.Utilities.Helper.FileHelper;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+        IFileHelper _fileHelper;
+
+        private string ImagesPath = "wwwroot\\Uploads\\Images\\";
+
+        public CarImageManager(ICarImageDal carImageDal, IFileHelper fileHelper)
+        {
+            _carImageDal = carImageDal;
+            _fileHelper = fileHelper;
+        }
+
+        public IResult Add(IFormFile file, Image carImage)
+        {
+            IResult result = BusinessRules.Run(CheckIfCarImageLimit(carImage.CarId));
+            if (result != null)
+            {
+                return result;
+            }
+            carImage.ImagePath = _fileHelper.Upload(file, ImagesPath);
+            carImage.Date = DateTime.Now;
+            _carImageDal.Add(carImage);
+            return new SuccessResult("Resim başarıyla yüklendi");
+        }
+
+        public IResult Delete(Image carImage)
+        {
+            _fileHelper.Delete(ImagesPath + carImage.ImagePath);
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Update(IFormFile file, Image carImage)
+        {
+            carImage.ImagePath = _fileHelper.Update(file, ImagesPath + carImage.ImagePath, ImagesPath);
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Image>> GetByCarId(int carId)
+        {
+            var result = BusinessRules.Run(CheckCarImage(carId));
+            if (result != null)
+            {
+                return new ErrorDataResult<List<Image>>(GetDefaultImage(carId).Data);
+            }
+            return new SuccessDataResult<List<Image>>(_carImageDal.GetAll(c => c.CarId == carId));
+        }
+
+        public IDataResult<Image> GetByImageId(int imageId)
+        {
+            return new SuccessDataResult<Image>(_carImageDal.Get(c => c.Id == imageId));
+        }
+
+        public IDataResult<List<Image>> GetAll()
+        {
+            return new SuccessDataResult<List<Image>>(_carImageDal.GetAll());
+        }
+        private IResult CheckIfCarImageLimit(int carId)
+        {
+            var result = _carImageDal.GetAll(c => c.CarId == carId).Count;
+            if (result > 5)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+        private IDataResult<List<Image>> GetDefaultImage(int carId)
+        {
+            List<Image> carImage = new List<Image>();
+            carImage.Add(new Image { CarId = carId, Date = DateTime.Now, ImagePath = "DefaultImage.jpg" });
+            return new SuccessDataResult<List<Image>>(carImage);
+        }
+        private IResult CheckCarImage(int carId)
+        {
+            var result = _carImageDal.GetAll(c => c.CarId == carId).Count;
+            if (result > 0)
+            {
+                return new SuccessResult();
+            }
+            return new ErrorResult();
+        }
+    }
+}
